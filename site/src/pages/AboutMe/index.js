fcffb6ef28aa1bb42bf30ebde2cbbed6e7b9852a
gwcew <git.matvey@yandex.ru>
2021-02-13 21:45:55 +0300

    ADD: final fixes

--- site/src/pages/AboutMe/index.js
+++ site/src/pages/AboutMe/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import HeaderComponent from '../../components/Header';
 import LabelToScrollComponent from '../../components/LabelToScroll';
 import PostComponent from '../../components/Post';
 
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      function updateSize() {
+        setSize([document.documentElement.clientWidth, 0]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+}
+
 function Page() {
+    const [width, height] = useWindowSize();
+
     return <div className="page-aboutme">
-        <HeaderComponent 
+        {width < 602 ? null :         <HeaderComponent 
         arrayTexts={["HOME", "ABOUT ME", "PORTFOLIO", "CONTACT"]}
         className="page-aboutme-header"
         classNameTable="page-aboutme-header-table"
-        classNameTableNested="page-aboutme-header-table-nested"></HeaderComponent>
+        classNameTableNested="page-aboutme-header-table-nested"></HeaderComponent>}
+
+
         <PostComponent
         headerTextName="ABOUT ME"
         authorTextName="Jason Wood"
@@ -19,9 +36,10 @@ function Page() {
         classContentHeaderName="page-aboutme-content-post-header"
         classContentFooterName="page-aboutme-content-post-footer"
         classContentBodyName="page-aboutme-content-post-body"></PostComponent>
-        <LabelToScrollComponent 
+
+        {width < 602 ? null :        <LabelToScrollComponent 
         text="KEEP SCROLLING, THERE IS STILL MORE TO COME"
-        className="page-aboutme-footer"></LabelToScrollComponent>
+        className="page-aboutme-footer"></LabelToScrollComponent>}
     </div>
 }
 
--- site/src/pages/Home/index.js
+++ site/src/pages/Home/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import LabelToScroll from '../../components/LabelToScroll';
 import HeaderComponent from '../../components/Header';
 
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      function updateSize() {
+        setSize([document.documentElement.clientWidth, 0]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+}
+
 function Page() {
+    const [width, height] = useWindowSize();
+
     return <div className="page-home">
+
         <HeaderComponent 
-        arrayTexts={["HOME", "ABOUT ME", "PORTFOLIO", "CONTACT"]}
+        arrayTexts={width < 602 ? ["∞ MENU"] : ["HOME", "ABOUT ME", "PORTFOLIO", "CONTACT"]}
         className="page-home-header"
         classNameTable="page-home-header-table"
         classNameTableNested="page-home-header-table-nested"></HeaderComponent>
-        <LabelToScroll text="SCROLL DOWN TO SEE MORE" className="page-home-footer"></LabelToScroll>
+
+        {width < 602 ? null : <LabelToScroll text="SCROLL DOWN TO SEE MORE" className="page-home-footer"></LabelToScroll>}
     </div>
 }
 
